522c407cc38e572685a281f9df37ff0429d44bce
shivam4401 <shivampoddar4401@gmail.com>
2025-10-17 14:22:09 +0530

    save all changes

--- frontend/src/context/ServerContex.jsx
+++ frontend/src/context/ServerContex.jsx
@@ -4,6 +4,7 @@ import { createContext } from "react";
 export const serverDataContext = createContext();
 const serverContex = ({ children }) => {
   const serverUrl = "http://localhost:3000";
+  // const serverUrl = "https://creditsea-assignment-backend.onrender.com";
   const value = { serverUrl };
   return (
     <div>
